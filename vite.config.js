b60bfb9b25598ccdf8a2f950b57f5de929784ff7
PRATIK SHETTI <pratikpshetti45@gmail.com>
2025-12-17 23:25:32 +0530

    Update base path to / for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/risinghelixx/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
